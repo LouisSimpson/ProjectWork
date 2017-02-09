ddd7caa8e245c7659849320081aed426c4319ad8
p14165711 <p14165711@my365.dmu.ac.uk>
2017-02-09 16:58:09 +0000

    changes to tstAddress and class library

---
+++ Test Framework/tstAddress.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Class_Library;
+
+namespace Test_Framework
+{
+    [TestClass]
+    public class tstAddress
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsAddress AnAddress = new clsAddress();
+            Assert.IsNotNull(AnAddress);
+        }
+    }
+}
